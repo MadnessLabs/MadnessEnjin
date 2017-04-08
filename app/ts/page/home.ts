66652a27abfb69c5fe18292f5b90c51dc9b64cea
Bobby Johnson <popcorn245@gmail.com>
2017-04-07 23:57:49 -0500

    Initial Commit

---
+++ app/ts/page/home.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../typings/index.d.ts"/>
+module MadnessEnjin {
+    'use strict';
+
+    class HomeController {
+        constructor(
+            
+        ) {
+            // ON LOAD       
+        }
+    }
+
+    angular.module('MadnessEnjin')
+           .controller('MadnessEnjin.HomeController', HomeController);
+}
